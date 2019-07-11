b049b95bc1f778712284ba32dc0456688c4e414a
alexeyshesh <50503204+alexeyshesh@users.noreply.github.com>
2019-07-11 22:20:16 +0300

    Add files via upload

---
+++ Tasks/sheshukov/PyTasks/9.py
@@ -0,0 +1,9 @@
+x1, y1, x2, y2 = map(int, input().split())
+
+if 1 <= x1 <= 8 and 1 <= y1 <= 8 and 1 <= x2 <= 8 and 1 <= y2 <= 8:
+    if abs(x1 - x2) == 1 and abs (y1 - y2) == 2:
+        print("YES")
+    else:
+        print("NO")
+else:
+    print("NO")
