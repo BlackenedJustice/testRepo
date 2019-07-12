90c1f6cda60c2cb736c418e9fea3b05b8db0a309
CatherineFish <kett_x@mail.ru>
2019-07-13 01:01:46 +0300

    Delete 9.py

--- 9.py
+++
@@ -1,8 +0,0 @@
-x_1 = int(input())
-y_1 = int(input())
-x_2 = int(input())
-y_2 = int(input())
-if abs(x_1 - x_2) == 1 and abs(y_1 - y_2) == 2 or abs(x_1 - x_2) == 2 and abs(y_1 - y_2) == 1:
-    print('YES')
-else:
-    print('NO')
