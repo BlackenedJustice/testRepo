198d74f14e660aa90233fa70cd23198432a88131
CatherineFish <kett_x@mail.ru>
2019-07-13 23:40:30 +0300

    Update 1.py

--- Tasks/Chekhonina/PyTasks/1.py
+++ Tasks/Chekhonina/PyTasks/1.py
@@ -1,5 +1,8 @@
-fib_1 = fib_2 = 1
-n = int(input()) - 2
-for i in range(n):
-    fib_1, fib_2 = fib_2, fib_1 + fib_2
-print(fib_2)
+def fib(n):
+    fib_1 = fib_2 = 1
+    for i in range(n - 2):
+        fib_1, fib_2 = fib_2, fib_1 + fib_2
+    return fib_2
+
+
+print(fib(int(input())))
