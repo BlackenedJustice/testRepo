25bdac570ca1e3249393ac2e6c6632469faaec5d
CatherineFish <kett_x@mail.ru>
2019-07-14 01:39:05 +0300

    Update 9.py

--- Tasks/Chekhonina/PyTasks/9.py
+++ Tasks/Chekhonina/PyTasks/9.py
@@ -1,7 +1,4 @@
-x_1 = int(input())
-y_1 = int(input())
-x_2 = int(input())
-y_2 = int(input())
+x_1, y_1, x_2, y_2 = map(int, input().split())
 if abs(x_1 - x_2) == 1 and abs(y_1 - y_2) == 2 or abs(x_1 - x_2) == 2 and abs(y_1 - y_2) == 1:
     print('YES')
 else:
