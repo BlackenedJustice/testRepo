33938559779b83af51af24ffcbeca11031a57633
CatherineFish <kett_x@mail.ru>
2019-07-14 01:32:07 +0300

    Update 13.py

--- Tasks/Chekhonina/PyTasks/13.py
+++ Tasks/Chekhonina/PyTasks/13.py
@@ -1,7 +1,2 @@
-h_1 = int(input())
-m_1 = int(input())
-s_1 = int(input())
-h_2 = int(input())
-m_2 = int(input())
-s_2 = int(input())
+h_1, m_1, s_1, h_2, m_2, s_2 = map(int, input().split())
 print(s_2 - s_1 + (m_2 - m_1) * 60 + (h_2 - h_1) * 3600)
