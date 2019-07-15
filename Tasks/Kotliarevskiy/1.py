d4863c410c3149836f09f8306d3f103194aea5ba
AK1737 <51711644+AK1737@users.noreply.github.com>
2019-07-15 14:09:00 +0300

    Update 1.py

--- Tasks/Kotliarevskiy/1.py
+++ Tasks/Kotliarevskiy/1.py
@@ -1,19 +1,14 @@
 def f():
     print('enter the number')
-    n=int(input())
-    if n < 3 :
-        n=1
-    else:
-        n=n-2
-        x=1
-        y=1
+    
+    
+        n=int(input())-1
+        x,y=1,1        
         for i in range(n):
-            x=x+y
-            y=x-y
+            x, y = y, x + y
     print(x)
 f()        
 
 
 
 f
-print('как в 5 строчек хз(')
